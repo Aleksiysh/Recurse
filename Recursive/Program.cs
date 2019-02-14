86eea09621196d9db60e0bc3db8ec8c733be0308
Aleksey <aleks-sh@mail.ru>
2019-02-14 21:50:13 +0300

    1

--- Recursive/Program.cs
+++ Recursive/Program.cs
@@ -1,15 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Recursive
 {
     class Program
     {
+        static int[] weigths = new int[]{2,5,6,2,4,7};
+
+        static void Evaluate(bool[] subset)
+        {
+            var delta = 0;
+            for (var i = 0; i < subset.Length; i++)
+                if (subset[i]) delta += weigths[i];
+                else delta -= weigths[i];
+            foreach (var e in subset)
+                Console.Write(e ? 1 : 0);
+            Console.Write(" ");
+            if (delta == 0)
+                Console.Write(" OK");
+            Console.WriteLine();
+
+        }
+
         static void Main(string[] args)
         {
+            Makesubsets(new bool[weigths.Length], 0);
+        }
+
+        static void Makesubsets(bool[] subset,int position)
+        {
+            if (position==subset.Length)
+            {
+                Evaluate(subset);
+                return;
+                
+
+            }
+            subset[position] = false;
+            Makesubsets(subset, position + 1);
+            subset[position] = true;
+            Makesubsets(subset, position + 1);
         }
     }
 }
